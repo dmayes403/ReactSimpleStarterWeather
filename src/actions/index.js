d1788de76a01b148df0c3237ba845ff6588c819e
dmayes403 <davidrmayes@gmail.com>
2017-08-20 17:22:50 -0600

    first http request made

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,13 +1,14 @@
 import axios from 'axios';
-import { API_key } from '../config.js';
+import API_key from '../config.js';
 const weather_key = API_key.keys[0].weather;
-const ROOT_URL = `api.openweathermap.org/data/2.5/forecast?appid=${weather_key}`
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${weather_key}`
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetchWeather(city) {
     const url = `${ROOT_URL}&q=${city},us`;
     const request = axios.get(url);
+    console.log(url);
 
     return {
         type: FETCH_WEATHER,
