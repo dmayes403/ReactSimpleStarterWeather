7cf63f0d868113313b8cb1a56ec930d45df9beab
dmayes403 <davidrmayes@gmail.com>
2017-08-21 22:38:54 -0600

    creating array of temps

--- src/containers/weather-list.js
+++ src/containers/weather-list.js
@@ -1,17 +1,41 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export default class WeatherList extends Component {
+class WeatherList extends Component {
+    renderWeather(cityData) {
+        const name = cityData.city.name;
+        const temps = cityData.list.map(weather => weather.main.temp);
+        console.log(temps);
+
+        return (
+            <tr key={name}>
+                <td>{ name }</td>
+            </tr>
+        )
+    }
+
     render() {
         return (
             <table className="table table-hover">
                 <thead>
                     <tr>
                         <th>City</th>
+                        <th>Temperature</th>
+                        <th>Pressure</th>
+                        <th>Humidity</th>
                     </tr>
                 </thead>
                 <tbody>
+                    {this.props.weather.map(this.renderWeather)}
+                    {/* {this.props.weather.map(item => this.renderWeather(item))} same thing as line above */} 
                 </tbody>
             </table>
         )
     }
 }
+
+function mapStateToProps({ weather }) { // this is saying pull the weather property off of state
+    return { weather } // { weather } === { weather: weather }
+}
+
+export default connect(mapStateToProps)(WeatherList);
