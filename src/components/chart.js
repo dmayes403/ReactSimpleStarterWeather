0ebf828e6ce4f5e778351f67c9174f7328d94e04
dmayes403 <davidrmayes@gmail.com>
2017-08-21 23:04:40 -0600

    charts have been added

---
+++ src/components/chart.js
@@ -0,0 +1,21 @@
+import _ from 'lodash';
+import React, { Component } from 'react';
+import { Sparklines, SparklinesLine, SparklinesReferenceLine } from 'react-sparklines';
+
+function average(data) {
+    return _.round(_.sum(data)/data.length);
+}
+
+// this component doesn't need to keep track of state sense data is being passed into it.
+// Because of this, it is a functional component and not a class based component.
+export default (props) => {
+    return (
+        <div>
+            <Sparklines height={120} width={180} data={props.data}>
+                <SparklinesLine color={props.color} />
+                <SparklinesReferenceLine type="avg" />
+            </Sparklines>
+            <div>{average(props.data)} ({props.units})</div>
+        </div>
+    )
+}
